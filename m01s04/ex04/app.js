f672d2350915b9dad1964ceca63dbcf2350f27be
Iulian <iuliancuculici@gmail.com>
2023-09-12 21:54:34 +0300

    m01s04 ex 1,3,4

---
+++ m01s04/ex04/app.js
@@ -0,0 +1,63 @@
+var limitA = 32;
+var limitB = 15;
+var denominator = 5;
+
+console.warn(
+  'Modifica exemplul astfel incat bucla sa numere de la 1 la 32, afisand in consola propozitii de forma “Numar pana la 32 inclusiv: i.”.',
+);
+
+for (var i = 0; i <= limitA; i++) {
+  console.log('Numar pana la ' + limitA + ' inclusiv: ' + i + '.');
+}
+
+console.warn(
+  'Modifica exemplul astfel incat bucla sa numere de la 1 la 31 (dar nu schimba numarul 32), afisand in consola propozitii de forma “Numar pana la 32 exclusiv: i',
+);
+
+for (var i = 0; i < limitA; i++) {
+  console.log('Numar pana la ' + limitA + ' exclusiv: ' + i + '.');
+}
+
+console.warn(
+  'Folosind keywordul break, opreste bucla atunci cand numarul este egal cu 15 (dar afiseaza-l), afisand propozitii de forma “Ma voi opri la 15: i.”.',
+);
+
+for (var i = 0; i < limitA; i++) {
+  console.log('Ma voi opri la ' + limitB + ': ' + i + '.');
+
+  if (i === limitB) {
+    console.log('M-am oprit la ' + limitB + '.');
+    break;
+  }
+}
+
+console.warn(
+  'Folosind keywordul continue, afiseaza doar numerele pare intre 1 si 32 (inclusiv 32 ) cu propozitii de forma “Toate numele pare intre 1 si 32 inclusiv: i.”.',
+);
+
+for (var i = 0; i <= limitA; i++) {
+  console.log(
+    'Toate numerele pare intre 1 si ' + limitA + ' inclusiv: ' + i + '.',
+  );
+  if (i % 2 === 0) {
+    continue;
+  }
+}
+
+console.warn(
+  'n mod similar, afiseaza doar numerele care sunt divizibile cu 5 cu propozitii de forma “Toate numerele divizibile cu 5 pana la 32 inclusiv: i.”. ',
+);
+
+for (var i = 1; i <= limitA; i++) {
+  if (i % 5 === 0) {
+    console.log(
+      'Toate numerele divizibile cu ' +
+        denominator +
+        ' pana la ' +
+        limitA +
+        ' inclusiv: ' +
+        i +
+        '.',
+    );
+  }
+}
